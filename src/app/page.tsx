181d6326466b656af3393327c9fce5126d0a5300
Pedro Sbeghen <pedrosbeghen@yahoo.com.br>
2024-01-09 05:59:58 -0300

    adapting to build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,15 +27,15 @@ export default function Home() {
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
             className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            href="https://www.tibia.com/community/?name=upback+yellowdrop"
             target="_blank"
             rel="noopener noreferrer"
           >
             By{' '}
             <Image
               src="/upback.png"
-              alt="Vercel Logo"
-              className="dark:invert"
+              alt="Upback Yellowdrop"
+              className="dark:invert w-full h-auto"
               width={200}
               height={48}
               priority
@@ -46,12 +46,12 @@ export default function Home() {
 
       <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert w-auto h-auto"
           src="/yellowdrop.png"
           alt="Yellow Drop"
-          width={720}
-          height={74}
-          priority
+          width={360}
+          height={24}
+          // priority
         />
       </div>
 
@@ -66,7 +66,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Valor de conversão em gold.
+            Valor de conversão em gold:
           </p>
           <input id="gc" type="text" value={gc} onChange={(e) => setGc(e.target.value)}  onKeyUp={(e) => {
             console.log(e);            
@@ -83,7 +83,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Valor do Tibia Coin.
+            Valor do Tibia Coin:
           </p>
           <input id="tc" type="text" value={tc} onChange={(e) => { setTc(e.target.value) }} onKeyUp={(e) => setTc(maskWithoutDecimal(tc))}/>
         </div>
@@ -98,7 +98,7 @@ export default function Home() {
             </span>
           </h2>
           <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Valor de venda x250TC em R$.
+            Valor de venda x250TC em R$:
           </p>
           <input id="vtc" type="number" value={vtc} onChange={(e) => { 
           setVtc(parseFloat(e.target.value));          
@@ -114,7 +114,9 @@ export default function Home() {
               -&gt;
             </span>
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}></p>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Valor em R$:
+          </p>
           <input id="tot" type="text" onChange={(e) => setTot(e.target.value)} />
         </div>
       </div>
